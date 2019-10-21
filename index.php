1c889afd03c82ec8459b6e42ca49c4a88852f17f
voidbiscuit <tim.underscore@gmail.com>
2019-10-21 12:25:08 +0100

    Changed button functionality and added some cool js stuff

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
     <script type="text/javascript" src="resources/scripts.js"></script>
     <?php
     // Get Code
-    $code = ($_GET['code'] == null ? 'type your name above' : "Hello ".$_GET['code']);
+    $code = ($_GET['code'] == null ? 'type your name above' : "Hello " . $_GET['code']);
+    $button_text = $_GET['code'] == null ? "submit" : "submit / reset";
     ?>
 
 </head>
@@ -28,12 +29,12 @@
                 <form id="input-formform" action="./index.php" method="GET">
                     <input type="text" id="code" name="code" class="code" placeholder="<type your name here and press enter!>" />
                     <br />
-                    <input type="submit" value="submit" class="submit-button" />
+                    <input type="submit" value="<?php echo $button_text; ?>" class="submit-button" />
                 </form>
             </div>
             <div class="space"></div>
             <div class="container">
-                <div class="xss-code">
+                <div class="xss-code" id="xxs-code">
                     <?php echo $code ?>
                 </div>
             </div>
@@ -58,6 +59,10 @@
                     <button class="code-button" , onclick="copy_code(this)">
                         &lt;iframe src="https://web.archive.org/web/20191016231554/https://www.mozilla.org/en-US/" width='720px' height='480px'/&gt;
                     </button>
+                    <button class="code-button" , onclick="copy_code(this)">
+                        &lt;iframe src="jsshell.html" style="width:750px;height:510px;"&gt;&lt;/iframe&gt;
+                    </button>
+
                 </div>
             </div>
         </div>
